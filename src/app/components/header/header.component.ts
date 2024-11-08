96db6a8bbeb060c82602c113db52e6a0c9ac1dcd
dragondevsimplify <dragondevsimplify@gmail.com>
2024-11-08 14:52:20 +0700

    Handle toggle main nav & profile nav

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,15 +7,42 @@ import { Component } from '@angular/core';
   imports: [CommonModule],
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {
-  showVerticalMenu = true
+export class HeaderComponent implements OnInit {
+  showMainNav = false
+  showProfileNav = false
   routeNameActivating = ''
+  MD_WIDTH = 768
 
-  toggleVerticalMenuShow() {
-    this.showVerticalMenu = !this.showVerticalMenu
+  ngOnInit() {
+    this.detectShowMainNav(window)
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize(e: UIEvent) {
+    const target = e.target as Window
+
+    if (!target) {
+      return
+    }
+
+    this.detectShowMainNav(target)
+  }
+
+  detectShowMainNav(w: Window) {
+    this.showMainNav = w.innerWidth >= this.MD_WIDTH
   }
 
   activeRoute(routeName: string) {
     this.routeNameActivating = routeName
+  }
+
+  // Only show toggle button on mobile
+  toggleShowMainNav() {
+    this.showMainNav = !this.showMainNav
+    this.showProfileNav = this.showMainNav
+  }
+
+  toggleShowProfileNav() {
+    this.showProfileNav = !this.showProfileNav
   }
 }
