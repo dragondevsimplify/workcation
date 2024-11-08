a29d68be0ff77e550c62e2d9c030ec5a45509a37
dragondevsimplify <dragondevsimplify@gmail.com>
2024-11-08 10:11:12 +0700

    Init common components

---
+++ src/app/components/destination-item/destination-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-destination-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './destination-item.component.html',
+  styleUrl: './destination-item.component.scss'
+})
+export class DestinationItemComponent {
+
+}
---
+++ src/app/components/destination-list/destination-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-destination-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './destination-list.component.html',
+  styleUrl: './destination-list.component.scss'
+})
+export class DestinationListComponent {
+
+}
