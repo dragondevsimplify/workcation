5bf24a652e108fc3abc8b41e9cecea5f3b51c759
dragondevsimplify <dragondevsimplify@gmail.com>
2024-11-08 11:30:49 +0700

    Update style user row

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
-  showVerticalMenu = false
+  showVerticalMenu = true
   routeNameActivating = ''
 
   toggleVerticalMenuShow() {
