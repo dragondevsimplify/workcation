e1e6a25910c7c7843f7cef8d78fefab606c21df1
dragondevsimplify <dragondevsimplify@gmail.com>
2024-11-09 11:51:03 +0700

    Update ui

--- src/app/components/destination-item/destination-item.component.ts
+++ src/app/components/destination-item/destination-item.component.ts
@@ -7,7 +7,7 @@ import { DestinationItem } from '../../models';
   imports: [],
   templateUrl: './destination-item.component.html',
   host: {
-    'class': 'mt-6 w-full lg:w-1/2 lg:px-4 xl:w-1/3',
+    'class': 'block mt-6 w-full lg:w-1/2 lg:px-4 xl:w-1/3',
   }
 })
 export class DestinationItemComponent {
